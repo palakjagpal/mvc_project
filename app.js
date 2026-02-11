7e56257a97f84d88f0da4ab5a8c595b95aae47c0
Palak Jagpal <palakjagpal70@gmail.com>
2026-02-11 19:04:35 +0530

    fix: remove .env from .gitignore and update server port handling in app.js

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const userRoutes = require('./routes/userRoutes');
 
-dotenv.config() //loading environment variables from .env file
-
 const app = express()
+
+const PORT = 3000;
 
 app.set("view engine", "pug")//setting pug as the view engine
 app.set("views", "./views") //setting the views directory
@@ -20,7 +20,7 @@ app.get("/", (req, res) => {
     res.render("home") //rendering the home.pug file when the user navigates to the root route
 });
 
-app.listen( process.env.PORT, ()=>{
-    console.log(`Server running in http://localhost:${process.env.PORT || 5000}`)
+app.listen( PORT || 5000, ()=>{
+    console.log(`Server running in http://localhost:${PORT}`)
 })
 
